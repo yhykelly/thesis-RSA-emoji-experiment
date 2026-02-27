b55c51c35d33a0f3ab765bbf242e8cc7239f07e3
Hening Wang <hening.wang0615@gmail.com>
2026-02-27 13:02:52 +0100

    add completion link for exp1a; enable prolific mode

--- experiments/exp1a/src/magpie.config.js
+++ experiments/exp1a/src/magpie.config.js
@@ -2,9 +2,9 @@ export default {
   experimentId: '55',
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
-  completionUrl: 'https://...',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CVZ2ZGX8',
   contactEmail: 'test@random.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'prolific',
   language: 'en'
 };
