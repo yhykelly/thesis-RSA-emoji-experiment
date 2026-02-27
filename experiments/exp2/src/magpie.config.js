d416018196e1e8274b080c2572c2176e28a7dc5f
Hening Wang <hening.wang0615@gmail.com>
2026-02-27 16:07:28 +0100

    Change exp1b and exp2 to prolific with the same completion link

--- experiments/exp2/src/magpie.config.js
+++ experiments/exp2/src/magpie.config.js
@@ -3,9 +3,9 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://...',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CVZ2ZGX8',
   contactEmail: 'test@random.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'prolific',
   language: 'en'
 };
