c7a90c56947f306be429380ccffcba8dc117a736
yhykelly <yhy_kelly@hotmail.com>
2026-03-04 15:01:33 +0100

    reset exp1a-2 to debug mode

--- experiments/exp1b/src/magpie.config.js
+++ experiments/exp1b/src/magpie.config.js
@@ -6,6 +6,6 @@ export default {
   completionUrl: 'https://app.prolific.com/submissions/complete?cc=CVZ2ZGX8',
   contactEmail: 'test@random.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'prolific',
+  mode: 'debug',
   language: 'en'
 };
