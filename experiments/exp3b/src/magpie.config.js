7ea86415aa401b24d2073d8113091cf8b66046c5
Hening Wang <hening.wang0615@gmail.com>
2026-03-25 22:33:28 +0100

    update config for exp 3a and 3b

--- experiments/exp3b/src/magpie.config.js
+++ experiments/exp3b/src/magpie.config.js
@@ -3,9 +3,9 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://...',
-  contactEmail: 'test@random.com',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CVZ2ZGX8',
+  contactEmail: 'hening.wang@outlook.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'debug',
+  mode: 'prolific',
   language: 'en'
 };
