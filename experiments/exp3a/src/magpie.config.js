cdb294e989957873659bc71331707da8445774ab
Hening Wang <hening.wang0615@gmail.com>
2026-03-25 22:56:40 +0100

    fix a typo

--- experiments/exp3a/src/magpie.config.js
+++ experiments/exp3a/src/magpie.config.js
@@ -3,7 +3,7 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CVZ2ZGX8,
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CVZ2ZGX8',
   contactEmail: 'hening.wang@outlook.com',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'prolific',
